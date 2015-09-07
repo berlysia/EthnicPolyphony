045190a0acfe76fc42f16b013eb5a3df61c8a41d
berlysia <berlysia@gmail.com>
2015-09-08 03:16:49 +0900

    add twitter api libraries

--- src/electron/index.js
+++ src/electron/index.js
@@ -1,6 +1,11 @@
 import app from 'app';
 import BrowserWindow from "browser-window";
 
+global.twitterAppCredentials = {
+  "consumer_key": process.env["TWITTER_CONSUMER_KEY"],
+  "consumer_secret": process.env["TWITTER_CONSUMER_SECRET"]
+};
+
 app.on('window-all-closed', () =>{
   if (process.platform != 'darwin')
     app.quit()
