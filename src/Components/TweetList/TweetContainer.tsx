a3ae9d8de537b54b23a9e62f28021ed285af1669
berlysia <berlysia@gmail.com>
2016-04-20 19:08:53 +0900

    fix(TweetList): improved performance by omitting state

--- src/Components/TweetList/TweetContainer.tsx
+++ src/Components/TweetList/TweetContainer.tsx
@@ -9,51 +9,38 @@ import {Result} from 'option-t/src/Result';
 const debug = require('remote').require('debug')('Components:TweetContainer');
 
 interface Props {
-  source_id: string;
-  status_id: string;
-  key: string;
-  appActions: ActionCreator;
+    source_id: string;
+    status_id: string;
+    key: string;
+    appActions: ActionCreator;
 }
 
-type State = {
-    tweet: Result<TweetModel, TweetStorageErr>;
-};
+type State = {};
 
 export default class TweetContainer extends React.Component<Props, State> {
     constructor(props: Props, context: any) {
         super(props, context);
-        this.state = {tweet: this.getTweet(props)};
     }
-    
+
     getTweet(props: Props): Result<TweetModel, TweetStorageErr> {
-        const getTweet_ : typeof getTweet = require('remote').require('./TweetStorage').getTweet;
+        const getTweet_: typeof getTweet = require('remote').require('./TweetStorage').getTweet;
         return getTweet_(props.source_id, props.status_id);
     }
-    
-    componentWillReceiveProps(props: Props) {
-        this.setState({tweet: this.getTweet(props)});
-    }
-    
+
     shouldComponentUpdate(nextProps: Props, nextState: State) {
-        if(this.props.source_id !== nextProps.source_id) return true;
-        if(this.props.status_id !== nextProps.status_id) return true;
-        if(this.state.tweet.isErr() || nextState.tweet.isErr()) return true;
-        
-        const currTweet = this.state.tweet.unwrap();
-        const nextTweet = nextState.tweet.unwrap(); 
-        if(currTweet.deleted !== nextTweet.deleted) return true;
-        if(currTweet.favorited !== nextTweet.favorited) return true;
-        if(currTweet.retweeted !== nextTweet.retweeted) return true;
+        if (this.props.source_id !== nextProps.source_id) return true;
+        if (this.props.status_id !== nextProps.status_id) return true;
 
         return false;
     }
-    
+
     render() {
         debug('#render');
-        if (this.state.tweet.isErr()) {
+        const tweetWrapper = this.getTweet(this.props);
+        if (tweetWrapper.isErr()) {
             return;
         }
-        const tweet = this.state.tweet.unwrap();
+        const tweet = tweetWrapper.unwrap();
         if (tweet.retweeted_status) {
             return (
                 <Tweet
@@ -62,7 +49,7 @@ export default class TweetContainer extends React.Component<Props, State> {
                     key={tweet.id_str}
                     appActions={this.props.appActions}
                     retweet_user={tweet.user}
-                />);
+                    />);
         } else {
             return (
                 <Tweet
@@ -70,7 +57,7 @@ export default class TweetContainer extends React.Component<Props, State> {
                     source_id={this.props.source_id}
                     key={tweet.id_str}
                     appActions={this.props.appActions}
-                />);
+                    />);
         }
     }
 }
